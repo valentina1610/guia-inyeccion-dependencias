b30e953b8a765733a1c7dfb78428671f3a02c36a
Valentina <valentinaolmosoo@gmail.com>
2025-07-19 11:17:12 -0300

    ej4 added

---
+++ ej4dependencias/ej4dependencias/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ej4dependencias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
